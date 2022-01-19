29295a22cbb2881de614b06aa7f25bbcc1db656d
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-01-19 17:39:55 +0900

    trouble shooting

--- src/components/Rweet.js
+++ src/components/Rweet.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useState } from "react";
 import { dbService, storageService } from "./../fbase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
